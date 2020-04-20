03101150d1a6ad93b5486fbe67f8a92b065d465b
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-04-20 21:14:35 +0200

    updated setup file

---
+++ SchoolRegister.Web/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using SchoolRegister.BAL.Entities;
+using SchoolRegister.ViewModels.DTOs;
+using SchoolRegister.ViewModels.Vms;
+using System.Linq;
+
+namespace SchoolRegister.Web.Configuration
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapperConfigurationExpression Mapping(
+            this IMapperConfigurationExpression configurationExpression)
+        {
+            Mapper.Initialize(mapper =>
+            {
+                mapper.CreateMap<AddOrUpdateSubjectDto, Subject>();
+                mapper.CreateMap<Group, GroupVm>();
+                mapper.CreateMap<Subject, SubjectVm>()
+                    .ForMember(
+                        dest => dest.TeacherName,
+                        x => x.MapFrom(
+                            src => $"{src.Teacher.FirstName} {src.Teacher.LastName}")
+                        )
+                    .ForMember(dest => dest.Groups,
+                        x => x.MapFrom(
+                            src => src.SubjectGroups.Select(y => y.Group)
+                            )
+                        );
+                mapper.CreateMap<SubjectVm, AddOrUpdateSubjectDto>();
+                mapper.CreateMap<Teacher, TeacherVm>();
+            });
+            return configurationExpression;
+        }
+    }
+}
