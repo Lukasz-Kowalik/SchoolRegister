8be47b7b4e519e7420994778d17075b77c7a93fc
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-04-25 22:10:53 +0200

    repaird project

---
+++ SchoolRegister.ViewModels/Vms/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolRegister.ViewModels.Vms
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
